a9444e13f2cc250b5e907ea995165e4214768eef
macbookair <nj.martin123@gmail.com>
2015-11-19 17:09:22 -0500

    Merge branch 'master' of https://github.com/TIY-iF/ironF
    # Conflicts:
    #	src/main/java/com/theironyard/controllers/ironFundController.java
    #	src/main/java/com/theironyard/entities/User.java

--- src/main/java/com/theironyard/controllers/ironFundController.java
+++ src/main/java/com/theironyard/controllers/ironFundController.java
@@ -1,19 +1,14 @@
 package com.theironyard.controllers;
+import com.theironyard.entities.Project;
 import com.theironyard.entities.User;
 import com.theironyard.services.ProjectRepo;
 import com.theironyard.services.UserRepo;
 import com.theironyard.util.PasswordHash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.theironyard.entities.User;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
-import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 
 /**
@@ -28,10 +23,11 @@ public class ironFundController {
     @Autowired
     ProjectRepo projects;
 
+
+
     @RequestMapping("/login")
     public User login(
             HttpSession session,
-            HttpServletResponse response,
             String username,
             String password
     ) throws Exception {
@@ -49,25 +45,53 @@ public class ironFundController {
     }
 
     @RequestMapping("/logout")
-    public void logout(HttpSession session, HttpServletResponse response) throws IOException {
+    public void logout(HttpSession session) throws IOException {
         session.invalidate();
     }
 
+    @RequestMapping ("/create")
+    public void addProject (
+          String title,
+          String description,
+          String finishDate,
+          double balance, double goal,
+          HttpSession session) throws Exception {
 
+        String username = (String) session.getAttribute("username");
+        if (username == null) {
+            throw new Exception("Not Logged in");
 
+        }
+        User user = users.findOneByUsername(username);
 
+        Project project = new Project();
+        project.title = title;
+        project.balance = balance;
+        project.description = description;
+        project.finishDate = LocalDateTime.parse(finishDate);
+        project.startDate = LocalDateTime.now();
+        project.goal = goal;
+        projects.save(project);
 
-    @RequestMapping ("/add-project")
-    public String addProject (String title,
-                              String description,
-                              LocalDateTime startDate,
-                              LocalDateTime finishDate,
-                              double balance, double goal,
-                              HttpSession session) throws Exception {
-        String username = (String)session.getAttribute("username");
-        if (username == null){
-           throw new Exception("Not Logged in");
+    }
+    @RequestMapping ("/edit")
+    public void editProject (
+            int id,
+            String title,
+            String description,
+            HttpSession session,
+            double goal)
+            throws Exception {
+        if (session.getAttribute("username") == null){
+            throw new Exception("Not Logged in");
         }
 
+        Project project = projects.findOne(id);
+        project.title = title;
+        project.description = description;
+        project.goal = goal;
+        projects.save(project);
+
+    }
     
 }
