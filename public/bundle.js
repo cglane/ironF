8f43b2a489ec6375c4ceee792a824bda4b1bc56b
Jet Balagtas <jetbalagtas@gmail.com>
2015-11-20 20:14:10 -0500

    hard coded login form in html for testing

--- public/bundle.js
+++ public/bundle.js
@@ -203,13 +203,12 @@ module.exports = Backbone.View.extend({
   el: '#layoutView',
   initialize: function () {
     var self = this;
-    console.log(HeaderView);
     var headerHTML = new HeaderView();
     var footerHTML = new FooterView();
-    var formHTML = new FormView();
     var ironFundCollection = new IronFundCollection();
     ironFundCollection.fetch().then(function () {
       var ironFundView = new IronFundView({collection: ironFundCollection});
+      var formHTML = new FormView({collection:ironFundCollection});
       self.$el.find('section').html();
       self.$el.find('header').html(headerHTML.render().el);
       self.$el.find('footer').html(footerHTML.render().el);
@@ -12955,17 +12954,57 @@ module.exports = {
 
   header: [
     '<nav class="navbar navbar-inverse navbar-fixed-top">',
-    '<a class="navbar-brand" href="#">Iron Fund</a>',
-    '<button type="button" class="btn btn-default navbar-btn navbar-right">Sign-in / Create Account</button>',
+    '<div class="container-fluid">',
+      // <!-- Brand and toggle get grouped for better mobile display -->
+      '<div class="navbar-header">',
+        '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">',
+          '<span class="sr-only">Toggle navigation</span>',
+          '<span class="icon-bar"></span>',
+          '<span class="icon-bar"></span>',
+          '<span class="icon-bar"></span>',
+        '</button>',
+        '<a class="navbar-brand" href="#">Iron Fund</a>',
+        '<ul class="nav navbar-nav">',
+        '<li class="active"><a href="#">About <span class="sr-only">(current)</span></a></li>',
+        '<li><a href="#">Add a Project</a></li>',
+        '</ul>',
+      '</div>',
+
+      // <!-- Collect the nav links, forms, and other content for toggling -->
+      '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">',
+        '<ul class="nav navbar-nav navbar-right">',
+          // '<li><a href="#">Link</a></li>',
+          '<li class="dropdown">',
+            '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Account <span class="caret"></span></a>',
+            '<ul class="dropdown-menu">',
+              '<li><a href="#">Action</a></li>',
+              '<li><a href="#">Another action</a></li>',
+              '<li><a href="#">Something else here</a></li>',
+              '<li role="separator" class="divider"></li>',
+              '<li><a href="#">Separated link</a></li>',
+            '</ul>',
+          '</li>',
+        '</ul>',
+      '</div>', //<!-- /.navbar-collapse -->
+    '</div>', //<!-- /.container-fluid -->
+'</nav>',
   ].join(""),
   footer: [
     '<div class="footer-links">',
     '<nav class="navbar navbar-inverse navbar-fixed-bottom">',
     '<a class="navbar-brand" href="#">Iron Fund</a>',
-    '<a href="#">Home</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
-    '<a href="#">Add a Project</a>',
+    '<ul class="nav navbar-nav">',
+    '<li class="active"><a href="#">About <span class="sr-only">(current)</span></a></li>',
+    '<li><a href="#">Add a Project</a></li>',
+    '</ul>',
     '</nav>',
     '</div>',
+  ].join(""),
+  login: [
+
+  ].join(""),
+  createuser: [
+
   ].join(""),
 };
 
